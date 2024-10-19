673c8691a451b13eadd416cc3fc8b72d37310f85
zmx <9329667+starjang@users.noreply.github.com>
2024-10-19 14:38:01 +0800

    - add json

---
+++ View/List/PoketRootView.swift
@@ -0,0 +1,18 @@
+//
+//  PoketRootView.swift
+//  PocketAmigo
+//
+//  Created by zmx on 2024/10/19.
+//
+
+import SwiftUI
+
+struct PoketRootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PoketRootView()
+}
