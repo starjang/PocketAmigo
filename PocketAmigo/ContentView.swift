17f162e79cee6f2d6c3b777a7daa0b7e6be051c7
小星 <9329667+starjang@users.noreply.github.com>
2024-10-19 11:33:46 +0800

    Add files via upload
    - init project

---
+++ PocketAmigo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PocketAmigo
+//
+//  Created by zmx on 2024/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PocketAmigo/PocketAmigoApp.swift
@@ -0,0 +1,18 @@
+//
+//  PocketAmigoApp.swift
+//  PocketAmigo
+//
+//  Created by zmx on 2024/10/19.
+//
+
+import SwiftUI
+
+@main
+struct PocketAmigoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+        }
+    }
+}
