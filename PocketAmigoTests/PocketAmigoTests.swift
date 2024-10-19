e83933ad919fdfdb24d71be46caa70053a7ac421
小星 <9329667+starjang@users.noreply.github.com>
2024-10-19 11:34:15 +0800

    Add files via upload
    - init project

---
+++ PocketAmigoTests/PocketAmigoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PocketAmigoTests.swift
+//  PocketAmigoTests
+//
+//  Created by zmx on 2024/10/19.
+//
+
+import Testing
+@testable import PocketAmigo
+
+struct PocketAmigoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
